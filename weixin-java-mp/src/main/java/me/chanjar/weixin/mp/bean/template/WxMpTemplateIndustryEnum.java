60fdf91ce7ed76336623d031d7c9a7d9c2064665
playersun <playersun@users.noreply.github.com>
2022-11-11 20:16:22 +0800

    :art: 修复模板消息行业枚举”其它“文字错误问题（官方文档和接口实际返回不一致）

--- weixin-java-mp/src/main/java/me/chanjar/weixin/mp/bean/template/WxMpTemplateIndustryEnum.java
+++ weixin-java-mp/src/main/java/me/chanjar/weixin/mp/bean/template/WxMpTemplateIndustryEnum.java
@@ -175,9 +175,9 @@ public enum WxMpTemplateIndustryEnum {
    */
   PRINTING("印刷", "印刷", 40),
   /**
-   * 其它 - 其它
+   * 其他 - 其他
    */
-  OTHER("其它", "其它", 41);
+  OTHER("其他", "其他", 41);
 
   /**
    * 主行业（一级行业）
@@ -205,10 +205,7 @@ public enum WxMpTemplateIndustryEnum {
         return industryEnum;
       }
     }
-    if (Objects.equals(firstClass, "其他") && Objects.equals(secondClass, "其他")) {
-      //微信返回的其他行业实际上为"其他",而非"其它",此处兼容处理
-      return OTHER;
-    }
+
     return null;
   }
 
