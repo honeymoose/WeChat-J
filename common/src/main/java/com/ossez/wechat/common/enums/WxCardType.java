84acfa03f7a651886dee60868384270a20024f28
YuCheng Hu <huyuchengus@gmail.com>
2023-01-27 05:43:34 -0500

    更新微信卡券的枚举类型

---
+++ common/src/main/java/com/ossez/wechat/common/enums/WxCardType.java
@@ -0,0 +1,25 @@
+package com.ossez.wechat.common.enums;
+
+/**
+ * 微信卡券
+ *
+ * @author YuCheng
+ */
+public enum WxCardType {
+    MEMBER_CARD("MEMBER_CARD"),
+    GROUPON("GROUPON"),
+    CASH("CASH"),
+    DISCOUNT("DISCOUNT"),
+    GIFT("GIFT"),
+    GENERAL_COUPON("GENERAL_COUPON");
+
+    private String code;
+
+    WxCardType(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
